ae5ab4d82c0cb3404869a749e83dbab0086bc932
NguyenKhoa-dev <S4xlamdong@gmail.com>
2022-11-09 15:07:03 +0700

    Update

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const btn = document.getElementById('btn-convert'),
 inputContent = document.getElementById('input-content'),
 QRimage = document.getElementById('QR-image'),
-result = document.getElementById('rsl');
+result = document.getElementById('rsl'),
+pending = document.getElementById('pending');
 
+var imgSrc;
 
 btn.onclick = () => {
     let value = inputContent.value;
     if (value) {
-        QRimage.src = `https://api.qrserver.com/v1/create-qr-code/?data=${value}&size=250x250`;
+        imgSrc = `https://api.qrserver.com/v1/create-qr-code/?data=${value}&size=250x250`;
+        QRimage.src = imgSrc;
         result.classList.add('show');
     }
 }
@@ -16,3 +19,21 @@ function clearInput() {
     inputContent.value = '';
     result.classList.remove('show');
 }
+
+async function copyPicture() {
+    try {
+      const response = await fetch(imgSrc);
+      const blob = await response.blob();
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          [blob.type]: blob
+        })
+      ]);
+      pending.classList.add('show');
+      setTimeout(() => {
+        pending.classList.remove('show');
+      }, 2000)
+    } catch (err) {
+      console.error(err.name, err.message);
+    }
+};
